298f66e68334248c8f16e09c86fed57e2fce329c
Dany Oliveira <danyoliveira.op@gmail.com>
2022-08-24 14:31:06 +0100

    TurnTicketDispenser - Tests

---
+++ Awnsers.md
@@ -0,0 +1,17 @@
+EXERCISE 1 - TirePressureMonitoringSystem
+	DURATION: 1:05H
+	
+	SOLID VIOLATION PRINCIPLES 
+	1# Single Responsibility 
+	2# Open Closed principle
+	3# Dependency Inversion
+	
+
+EXERCISE 3 - TurnTicketDispenser
+	DURATION: 01:00H
+	
+	SOLID PROBLEMS
+	3# Dependency Inversion
+
+	
+	
---
+++ RefactorMircoExcercise/src/TurnTicketDispenser.Tests/TurnTicketDispenserTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace TDDMicroExercises.TurnTicketDispenser.Tests {
+    public class TurnTicketDispenserTests {
+        [Test]
+        public void TurnTicketInicialTest()
+        {
+            var ticketDispenser = new TicketDispenser();
+
+            var getTurnTicket = ticketDispenser.GetTurnTicket();
+
+            Assert.NotZero(getTurnTicket.TurnNumber);
+        }
+
+        [Test]
+        public void TurnTicketDifferentDispenserTest()
+        {
+            var ticketDispenser = new TicketDispenser();
+            var getTurnTicket = ticketDispenser.GetTurnTicket();
+
+            Assert.AreNotEqual(getTurnTicket.TurnNumber, getTurnTicket.TurnNumber);
+            Assert.AreNotEqual(getTurnTicket.TurnNumber, getTurnTicket.TurnNumber);
+            Assert.AreNotEqual(getTurnTicket.TurnNumber, getTurnTicket.TurnNumber);
+            Assert.AreNotEqual(getTurnTicket.TurnNumber, getTurnTicket.TurnNumber);
+        }
+    }
+}
